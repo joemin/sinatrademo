91dfd783f7e740ea8e8d256912e3e4d5828023b7
Joseph Min <josephmin@gmail.com>
2014-05-19 20:51:23 -0400

    numbers and list

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  toPrint = ""
+  counter = 1
+  File.open('cake.list', 'r') do |f1|
+    while temp = f1.gets
+      toPrint = toPrint + counter.to_s + ") " + temp + "<br>"
+      counter += 1
+    end
+  end
+  toPrint
 end
