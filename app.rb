5de6686ab0c8c4c17605fca73a5d129683bb1804
Joseph Min <josephmin@gmail.com>
2014-05-28 11:17:35 -0400

    accesses database now

--- app.rb
+++ app.rb
@@ -1,20 +1,25 @@
 require 'sinatra'
 require 'json'
+require 'data_mapper'
+require 'dm-migrations'
 
 get '/' do
   File.read('cakes.html')
 end
 
 get '/cakeList.json' do
-  content_type :json
-  arr = Array.new
-  toPrint = ""
+  DataMapper.setup(:default, 'sqlite:///tmp/test.db')
 
-  File.open('cake.list', 'r') do |f1|
-    while temp = f1.gets
-      arr << temp.strip
-    end
+
+  class Cake
+    include DataMapper::Resource
+
+    property :id,         Serial    # An auto-increment integer key
+    property :name,       String    # A varchar type string, for short strings
   end
 
+  DataMapper.finalize
+
+  arr = Cake.all
   arr.to_json
 end
